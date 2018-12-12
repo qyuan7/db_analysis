31f4ce94f32155f1e5e1ac8efc3ebfe6c9c106ff
qyuan7 <qyuan@ic.ac.uk>
2018-12-12 14:32:10 +0000

    Corrected post_processing_smiles for mismatching SMILE-mol-fingerprints Added analysis function for generated molecules in DA_DB_cluster_analysis removed problematic csv result

--- post_process_smiles.py
+++ post_process_smiles.py
@@ -51,10 +51,16 @@ def main():
     ori_df = pd.read_csv('./sampled_da_info/refined_smii.csv',header=None)
     ori_list = ori_df[0].tolist()
     frames = []
+    gen_mols = []
+    gen_fps = []
     for i in [1024, 2048, 4096, 8192, 16384, 32768]:
         gen_df = pd.read_csv('./sampled_da_info/sampled_da'+str(i)+'_smi.csv', header=None)
         gen_list = gen_df[0].tolist()
         over, num, smi_list = get_smi_list_overlap(ori_list, gen_list)
+        smi_mols = get_mols(smi_list)
+        smi_fps, failed_mols = get_fingerprints(smi_mols)
+        for idx in sorted(failed_mols, reverse=True):
+            del smi_list[idx]
         smi_df = pd.Series(data=smi_list, name='SMILES').to_frame()
         smi_df.loc[:,'Group'] = i
         frames.append(smi_df)
@@ -63,16 +69,22 @@ def main():
     #    total_unique_list.extend([smi_list])
 
     unique_df = pd.concat(frames)
+    print(len(unique_df))
     gen_smi = unique_df['SMILES'].tolist()
+    #for idx in sorted(failed_mols, reverse=True):
+    #    del gen_smi[idx]
+    #    del gen_mols[idx]
     gen_mols = get_mols(gen_smi)
     gen_fps, _ = get_fingerprints(gen_mols)
-    gen_gaps = predict_property('gbdt_regessor_gap.joblib', gen_fps)
-    gen_dips = predict_property('gbdt_regessor_dip.joblib', gen_fps)
-    gaps_series = pd.Series(data=gen_gaps)
-    dip_series = pd.Series(data=gen_dips)
-    unique_df.loc[:, 'Gap'] = gaps_series
-    unique_df.loc[:, 'Dip'] = dip_series
-    unique_df.to_csv('unique_sampled_smiles.csv', index=False)
+    print(len(gen_fps))
+    unique_df['Gaps'] = predict_property('gbdt_regessor_gap.joblib', gen_fps)
+    unique_df['Dips'] = predict_property('gbdt_regessor_dip.joblib', gen_fps)
+    #gaps_series = pd.Series(data=gen_gaps)
+    #dip_series = pd.Series(data=gen_dips)
+
+    #unique_df.loc[:, 'Gap'] = gaps_series
+    #unique_df.loc[:, 'Dip'] = dip_series
+    unique_df.to_csv('unique_sampled_smiles_corr.csv', index=False)
 
 
 if __name__ == '__main__':
